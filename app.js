bcbddbd4022e69e0cea2a91b61d0ffaab9af2002
Brendon Fatland <b.fatland@gmail.com>
2018-07-16 21:41:09 -0400

    fixed the nav bar

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
 
 // Need to get some Javascript or Jquery working that highlights the current tab.
-  /*  $("#NOrange").click(function(e) {
+/*    $("#NOrange").click(function(e) {
             //prevents form from submitting.
             e.preventDefault();
   $(".").removeClass("important highlight");
    $("#NOrange").addClass("important highlight");
         });
-  */
+*/
 });
 
 function openColor(selectedColor) {
-    var i, tabs, x;
+    var i, x;
+
     var x = document.getElementsByClassName("color");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
 
     document.getElementById(selectedColor).style.display = "block";
+
 
 }
